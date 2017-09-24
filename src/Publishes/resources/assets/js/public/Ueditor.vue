fa9beed061dcaefa8e17fa6013b644765c4c6bd3
zsping1989 <214986304@qq.com>
2017-09-24 11:34:33 +0800

    百度编辑器组件

---
+++ src/Publishes/resources/assets/js/public/Ueditor.vue
@@ -0,0 +1,73 @@
+<template>
+    <script :id="'ueditor_'+id" name="content" type="text/plain">
+        {{value}}
+    </script>
+</template>
+<script>
+    window.UEDITOR_HOME_URL = '/ueditor/';
+    require('./ueditor.config');
+    require('ueditor/example/public/ueditor/ueditor.all.min');
+    export default {
+        data(){
+            return {
+                changeing:false
+            };
+        },
+        props:{
+            id:{
+                type: [String]
+            }, //绑定值
+            value: {
+                type:[String, Number],
+                default: function () {
+                    return '';
+                }
+            },
+            disabled:{
+                default: function () {
+                    return false;
+                }
+            }
+        },
+        methods:{
+
+
+        },
+        computed:{
+
+        },
+        watch:{
+            value(value,oldValue){
+                if(!this.changeing){
+                    this.editor.setContent(value || "");
+                    this.editor.focus(true);
+                    this.editor.blur();
+                }
+                this.changeing = false;
+            },
+            disabled(value,oldValue){
+                if(value){
+                    this.editor.setDisabled('fullscreen');
+                }else {
+                    this.editor.setEnabled();
+                }
+            }
+        },
+        mounted() {
+            var $this = this;
+            this.editor = UE.getEditor('ueditor_'+this.id);
+            if(this.disabled){
+                this.editor.setDisabled('fullscreen');
+            }
+            this.editor.addListener('contentChange', function(){
+                var value = $this.editor.getContent();
+                $this.changeing = true;
+                $this.$emit('input', value); //修改值
+                $this.$emit('change',value); //修改值
+            });
+       },
+        updated(){
+
+        }
+    }
+</script>
