dd24a92ad8458072314e117fcb007316dc624557
shiping.zhang <shiping.zhang@1905.com>
2017-11-02 11:44:24 +0800

    用户激活中间件

---
+++ src/Middleware/ActivatedMiddleware.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace LaravelAdmin\Middleware;
+
+use App\Models\Log;
+use App\Models\Menu;
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class ActivatedMiddleware{
+    /**
+     * 脚本运行时调用
+     *
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+
+    public function handle($request, Closure $next)
+    {
+        $user = Auth::user();
+        if($user['status']==0){
+            dd('请先通过用户注册的邮箱激活');
+        }
+        $response = $next($request);
+        //后置操作
+        return $response;
+    }
+
+
+    /**
+     *
+     * 结果返回到客户端后调用
+     *
+     * Handle an incoming request.
+     *
+     * param  \Illuminate\Http\Request  $request
+     * param  \Closure  $next
+     * return mixed
+     */
+    public function terminate($request, $response)
+    {
+
+
+
+    }
+}
