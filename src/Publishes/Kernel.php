6d42f3649bc69b7f600abfa7511e7b1eef325c10
zsping1989 <214986304@qq.com>
2017-11-05 20:47:17 +0800

    测试

--- src/Publishes/Kernel.php
+++ src/Publishes/Kernel.php
@@ -56,6 +56,8 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'admin' => \LaravelAdmin\Middleware\AdminMiddleware::class
+        'admin' => \LaravelAdmin\Middleware\AdminMiddleware::class,
+        'activated' => \LaravelAdmin\Middleware\ActivatedMiddleware::class
+
     ];
 }
