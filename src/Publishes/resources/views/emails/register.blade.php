c1b46421b16f8bfc5f94496a212507ce86a6cf5d
zsping1989 <214986304@qq.com>
2017-11-05 23:02:20 +0800

    邮件

--- src/Publishes/resources/views/emails/register.blade.php
+++ src/Publishes/resources/views/emails/register.blade.php
@@ -3,7 +3,7 @@
 {{array_get($user,'name',array_get($user,'uname'))}} 您好:<br/>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;您用 {{ config('app.name', 'LaravelAdmin') }} 注册了本站账号
 <br/>
-请在1 小时内点击此链接以完成绑定 {{$url}}
+请在1 小时内点击此链接以完成激活 {{$url}}
 
 @component('mail::button', ['url' => $url])
 激活用户
