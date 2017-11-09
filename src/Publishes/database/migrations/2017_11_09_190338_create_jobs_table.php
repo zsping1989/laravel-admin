513fd8b6665153352751d47fe059a8e96972c140
shiping.zhang <shiping.zhang@1905.com>
2017-11-09 19:08:30 +0800

    测试

---
+++ src/Publishes/database/migrations/2017_11_09_190338_create_jobs_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class CreateJobsTable extends Migration
+{
+
+    protected $bindModel='App\Models\Job';
+
+    /**
+     * Run the migrations.
+     *
+     * 返回: void
+     */
+    public function up()
+    {
+        $model = new $this->bindModel();
+        $prefix = $model->getConnection()->getTablePrefix();
+        $connection = $model->getConnectionName()?: config('database.default');
+        DB::connection($connection)->statement("CREATE TABLE IF NOT EXISTS `".$prefix.$model->getTable()."` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `queue` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `attempts` tinyint(3) unsigned NOT NULL,
+  `reserved_at` int(10) unsigned DEFAULT NULL,
+  `available_at` int(10) unsigned NOT NULL,
+  `created_at` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `jobs_queue_index` (`queue`(250))
+) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return  void
+     */
+    public function down()
+    {
+        $model = new $this->bindModel();
+        $prefix = $model->getConnection()->getTablePrefix();
+        $connection = $model->getConnectionName()?: config('database.default');
+        DB::connection($connection)->statement('DROP TABLE IF EXISTS `'.$prefix.$model->getTable().'`;');
+    }
+}
